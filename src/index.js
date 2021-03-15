d91b2c41e1851c69faf28e53e62a30212639beb6
Regina Celi da Silva <regicelis7@hotmail.com>
2021-03-15 15:37:18 -0300

    completed middlewares

--- src/index.js
+++ src/index.js
@@ -10,19 +10,73 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers
+
+  const user = users.find((user)=> user.username === username)
+  
+  if(!user){
+    return response.status(404).json({error: "User not found."})
+  }
+
+  request.user = user
+
+  return next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const {user} = request
+
+  const lessThanLimit = (user.todos.length < 10) ? true : false
+
+  if( (user.pro===false) && (lessThanLimit === false)){
+    return response.status(403).json({error: "Limit exceeded",
+    })
+  }
+
+    return next();  
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+
+  const {username} = request.headers
+  const {id}= request.params
+  const validateUUID = validate(id, 4)
+
+  
+  if(!validateUUID){
+    return response.status(400).json()
+  }
+
+  const user = users.find((user)=>user.username === username)
+
+  if(!user){ 
+    return response.status(404).json({error: "Username not found."})
+  }
+
+  const todo = user.todos.find((todo)=> todo.id === id)
+
+  if(!todo){
+    return response.status(404).json({error: "Todo not found."})
+  }
+  
+  request.user = user
+  request.todo = todo
+
+  return next()
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const {id}= request.params
+
+  const user = users.find((user)=> user.id === id)
+
+  if(!user){
+    return response.status(404).json({error: "User not found."})
+  }
+
+  request.user = user
+  return next()
 }
 
 app.post('/users', (request, response) => {
